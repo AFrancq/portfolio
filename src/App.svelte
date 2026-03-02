5cb7d21194f9ff0b858454882dfd3ff1a36ae4c8
Ash Francq <ash.francq@gmail.com>
2026-03-02 12:50:39 -0600

    Logos

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,99 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import svelteLogo from "./assets/svelte.svg";
+    import pythonLogo from "./assets/python.svg";
+    import rustLogo from "./assets/rust.svg";
+    import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <aside class="sidebar">
+        <nav>
+            <ul>
+                <li>
+                    <a
+                        href="https://svelte.dev"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img
+                            src={svelteLogo}
+                            class="logo svelte"
+                            alt="Svelte Logo"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://python.org"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img
+                            src={pythonLogo}
+                            class="logo python"
+                            alt="Python Logo"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://rust-lang.org"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={rustLogo} class="logo rust" alt="Rust Logo" />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+    <!-- <div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+            <img src={viteLogo} class="logo" alt="Vite Logo" />
+        </a>
+        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+            <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+        </a>
+    </div> -->
+    <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+    <div class="card">
+        <Counter />
+    </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+    <p>
+        Check out <a
+            href="https://github.com/sveltejs/kit#readme"
+            target="_blank"
+            rel="noreferrer">SvelteKit</a
+        >, the official Svelte app framework powered by Vite!
+    </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <p class="read-the-docs">
+        Click on the Vite and Svelte logos to learn more
+    </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    .logo {
+        height: 6em;
+        padding: 1.5em;
+        will-change: filter;
+        transition: filter 300ms;
+    }
+    .logo:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    .logo.python:hover {
+        filter: drop-shadow(0 0 2em #ffe261);
+    }
+    .logo.rust:hover {
+        filter: drop-shadow(0 0 2em #e8e6e3);
+    }
+    .logo.svelte:hover {
+        filter: drop-shadow(0 0 2em #ff3e00aa);
+    }
+    .read-the-docs {
+        color: #888;
+    }
 </style>
