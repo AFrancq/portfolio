903c71b54d5d0985c52ff5514996cadbbb8eca17
Ash Francq <ash.francq@gmail.com>
2026-03-04 12:12:29 -0600

    Sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -47,15 +47,7 @@
             </ul>
         </nav>
     </aside>
-    <!-- <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} class="logo" alt="Vite Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-            <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-        </a>
-    </div> -->
-    <h1>Vite + Svelte</h1>
+    <h1>Home</h1>
 
     <div class="card">
         <Counter />
